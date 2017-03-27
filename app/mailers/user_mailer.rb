d454b9478a78e0304859263fee00cb786006f685
Satu Asikainen <admiralmoonkey@gmail.com>
2017-03-27 11:12:18 +0300

    3.1 defined contact_form in user_mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,9 @@
 class UserMailer < ApplicationMailer
+  default from: "contact@valpurisprintshop.com"
+
+  def contact_form(email, name, message)
+    mail(:from => email),
+      :to => 'admiralmoonkey@gmail.com'
+      :subject => "A new contact form message from #{name}")
+  end
 end
