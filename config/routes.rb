d6a0dde537e1c64234da0e7641a3c4e0136c8fee
Satu Asikainen <admiralmoonkey@gmail.com>
2017-03-28 14:34:32 +0300

    3.1 authentication login/logout and sign up created

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
   resources :users
   resources :orders, only: [:index, :show, :create, :destroy]
   resources :products
+
   get 'static_pages/about'
 
   get 'static_pages/contact'
