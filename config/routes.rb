66c18898a71862d11a8e0c918ca73d61dccf66d7
Satu Asikainen <admiralmoonkey@gmail.com>
2017-03-30 12:12:45 +0300

    3.2 created review model and associations

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 
 Rails.application.routes.draw do
+  resources :comments
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
   resources :users
   resources :orders, only: [:index, :show, :create, :destroy]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170328084808) do
+ActiveRecord::Schema.define(version: 20170330090048) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "product_id"
+    t.text     "review"
+    t.integer  "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
