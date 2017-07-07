4e22696747cd9dc2df7d88415d6954abc34b660b
Satu Asikainen <admiralmoonkey@gmail.com>
2017-07-07 09:11:36 +0300

    Installed ElevateZoom jquery to product images

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -7,9 +7,15 @@ $(document).on('turbolinks:load', function(){
     return $(this).attr('data-score');
     }
   });
-});
 
-$(document).ready(function() {
+  $('.img-zoom').elevateZoom({
+      zoomWindowFadeIn: 500,
+      zoomWindowFadeOut: 500,
+      lensFadeIn: 500,
+      lensFadeOut: 500,
+      zoomWindowPosition: 1,
+      zoomWindowOffetx: 50});
+
   $(window).on('scroll', function() {
       var scrollTop = $(this).scrollTop();
 
@@ -24,3 +30,19 @@ $(document).ready(function() {
       });
   });
 });
+//
+// $(document).ready(function() {
+//   $(window).on('scroll', function() {
+//       var scrollTop = $(this).scrollTop();
+//
+//       $('#navigation-header').each(function() {
+//         var topDistance = $(this).offset().top;
+//
+//         if ((topDistance-60) <= scrollTop ) {
+//             $('#navigation-header').addClass('navigationTop');
+//             $('.content').css('margin-top', '60px')
+//             $('.content').css('margin-bottom', '120px')
+//         }
+//       });
+//   });
+// });
