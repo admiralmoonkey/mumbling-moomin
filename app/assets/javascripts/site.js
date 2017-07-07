a8a0b81e1ebab95404b1ec0ab9878cda926befd0
Satu Asikainen <admiralmoonkey@gmail.com>
2017-07-07 09:59:23 +0300

    ajax update commenting added

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,4 +1,4 @@
-$(document).on('turbolinks:load', function(){
+var refreshRating = function(){
   $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
 
   $('.rated').raty({ path: '/assets',
@@ -7,6 +7,10 @@ $(document).on('turbolinks:load', function(){
     return $(this).attr('data-score');
     }
   });
+};
+
+$(document).on('turbolinks:load ajaxSuccess', function(){
+  refreshRating();
 
   $('.img-zoom').elevateZoom({
       zoomWindowFadeIn: 500,
